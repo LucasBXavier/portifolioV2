756366fd0cf0d984706074abcd7141f4992e20e5
Lucas <lucas_b.xavier@hotmail.com>
2026-01-16 00:46:08 -0300

    feat: add project data for portfolio, update contact information, and enhance responsiveness in styles

---
+++ js/data/projects.data.js
@@ -0,0 +1,68 @@
+const projetos = [
+    {
+        id: 1,
+        nome: "Portfólio Pessoal",
+        descricao: "Um site para mostrar meus projetos e habilidades como desenvolvedor web.",
+        link: "https://lucasboareto.vercel.app/",
+        imagem: "portfolio.png",
+        tecnologias: ["HTML", "CSS", "JavaScript"]
+    },
+    {
+        id: 2,  
+        nome: "Método LD",
+        descricao: "Site voltado para venda de cursos online sobre desenvolvimento pessoal e profissional.",
+        link: "https://vidasemclt.com.br/",
+        imagem: "metodoLD.png",
+        tecnologias: ["HTML", "CSS", "JavaScript"]
+    },
+    {
+        id: 3,
+        nome: "ToDo List API Rest", 
+        descricao: "Uma API RESTful para gerenciar tarefas em uma lista de afazeres.",
+        link: "https://github.com/LucasBXavier/ToDoList",
+        imagem: "todo.png",
+        tecnologias: ["Spring", "Java", "H2 Database", "Lombok"]
+    },
+    {
+        id: 4,
+        nome: "LGPD",
+        descricao: "Site institucional para falar sobre a Lei Geral de Proteção de Dados.",
+        link: "https://rhoanbarioni.github.io/LGPD_Facul/index.html",
+        imagem: "lgpd.png",
+        tecnologias: ["HTML", "SCSS", "JavaScript"]
+    },
+    {
+        id: 5,
+        nome: "Busca CEP",
+        descricao: "Uma aplicação simples para buscar endereços a partir do CEP utilizando uma API externa.",
+        link: "https://lucasbxavier.github.io/busca-CEP/",
+        imagem: "buscacep.png",
+        tecnologias: ["HTML", "CSS", "JavaScript"]
+    },
+    {
+        id: 6,
+        nome: "GastroInfo",
+        descricao: "Um site informativo sobre saúde digestiva e dicas para manter um sistema digestivo saudável.",
+        link: "https://lucasbxavier.github.io/GastroInfo/",
+        imagem: "gastroinfo.png",
+        tecnologias: ["HTML", "CSS", "JavaScript"]
+    },
+    {
+        id: 8,
+        nome: "Spring Cloud",
+        descricao: "Projeto de microsserviços utilizando Spring Cloud para demonstrar comunicação entre serviços.",
+        link: "https://github.com/LucasBXavier/springCloud",
+        imagem: "springCloud.png",
+        tecnologias: ["Spring", "Java", "Spring Cloud", "Lombok", "Eureka", "Feign", "Spring Cloud Gateway", "RestTemplate"]
+    },
+    {
+        id: 9,
+        nome: "Screen Sound",
+        descricao: "Sistema de cadastro de musicas e albuns com avaliação dos usuários.",
+        link: "https://github.com/LucasBXavier/Screen-Sound",
+        imagem: "sistemaCadastro.png",
+        tecnologias: ["C#"]
+    }
+];
+
+export default projetos;
